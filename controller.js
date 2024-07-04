5dc0c3f9e06ebb4ea2bf7ca7099387bd86cae408
KleyversonO <kleyversonoliveira@gmail.com>
2024-07-04 07:59:26 -0300

    Iniciação do uso do Javascript fazendo com que haja a validação se existe algo escrito nos campos necessarios do projeto

---
+++ controller.js
@@ -0,0 +1,12 @@
+//VALIDAÇÃO DE PREENCHIMENTO
+function acessar(){
+    let loginEmail = document.getElementById('loginEmail').value
+    let loginSenha = document.getElementById('loginSenha').value
+
+    if(!loginEmail || !loginSenha){
+        alert('Favor preencher todos os campos')
+    }else{
+        //alert("Campos preenchidos com sucesso")
+        window.location.href = 'cadastro.html';
+    }
+}
