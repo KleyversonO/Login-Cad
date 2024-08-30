1333b988254d8eb8c1504c31e19c3e4acc8f5e4e
KleyversonO <kleyversonoliveira@gmail.com>
2024-08-30 07:23:36 -0300

    Atualização da parte de CPF

--- controller.js
+++ controller.js
@@ -17,6 +17,13 @@ function salvarUser() {
 function acessar() {
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
+    document.getElementById('cpfForm').addEventListener('submit', function(event){
+        event.preventDefault();
+     
+    }
+    );
+    
+
 
     if (!loginEmail || !loginSenha) {
         alert('Favor preencher todos os campos');
@@ -65,7 +72,7 @@ function salvarUser() {
 // Esse código tem a função de fazer uma tabela no projeto para que armazene e mostre o nome do usuário, e suas ações. Também à a variável responsável por pegar o último nome escrito para que ele fique amostra. Também tem o botão com a fumção para excluir e editar
 //lenght é uma propriedade que calcula o tamanho da lista. tr e td é a propriedade responsável por parte do processo de criação de lista e tabela
 function criarLista() {
-    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Email</th><th>Ações</th></tr>";
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Email</th><th>CPF</th><th>Ações</th></tr>";
     for (let i = 0; i <= (dadosLista.length - 1); i++) { // A propriedade length tem como responsabilidade retornar a quantidade de caracteres de uma string ou o tamanho de um array. Caso a string ou o array esteja vazio, é retornado o valor 0.
         tabela += "<tr><td>" + dadosLista[i] + "</td><td>" + emailLista[i] + "</td><td>  <button onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button> <button onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button> </td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
@@ -87,3 +94,50 @@ function editar(i) {
     dadosLista.splice(dadosLista[(i - 1)], 1);
 }
 // TEM A FUNÇÃO DE VALIDAR O EMAIL ANTES DE ENVIA-LO PARA O PROJETO.
+
+//Validação do CPF do projeto
+
+
+function validarCPF(cpf){
+    cpf = cpf.replace(/[^\d]+/g, ''); // Remove caracteres não numéricos
+ 
+    // Estrutura de decisão para verificar quantidade de dígitos e se todos os digitos são iguais
+    if(cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)){
+        return false;
+    }
+   
+    let soma = 0;
+    let resto;
+ 
+    // Validando o primeiro digito verificador
+    for(let i=1;i <= 9;i++){
+        soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
+    }
+ 
+    resto = (soma * 10) % 11;
+ 
+    if((resto === 10) || (resto === 11)){
+        resto = 0;
+    }
+    if(resto !== parseInt(cpf.substring(9, 10))){
+        return false;
+    }
+ 
+    soma = 0;
+    // Validando o segundo digito verificador
+    for(let i = 1; i <= 10; i++){
+        soma += parseInt(cpf.substring(i-1, i) * (12 - i));
+    }
+ 
+    resto = (soma * 10) % 11;
+ 
+    if((resto === 10) || (resto === 11)){
+        resto = 0;
+    }
+   
+    if(resto !== parseInt(cpf.substring(10, 11))){
+        return false;
+    }
+ 
+    return true;
+}
